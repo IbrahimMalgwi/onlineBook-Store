4fb6a1b1d2b8207fb2a4821b9189cfad921afbf7
IbrahimGanaMalgwi <ibrahimganamalgwi@outlook.com>
2023-06-18 06:31:47 +0100

    refactored

--- src/main/java/com/ganzymalgwi/online/bookstore/data/model/Book.java
+++ src/main/java/com/ganzymalgwi/online/bookstore/data/model/Book.java
@@ -1,19 +1,23 @@
 package com.ganzymalgwi.online.bookstore.data.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
     private BigDecimal price;
     private String description;
+    private LocalDateTime createTime;
 }
--- src/main/java/com/ganzymalgwi/online/bookstore/data/model/Purchase.java
+++ src/main/java/com/ganzymalgwi/online/bookstore/data/model/Purchase.java
@@ -1,16 +1,21 @@
 package com.ganzymalgwi.online.bookstore.data.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
+@Getter
+@Setter
 @Entity
+@Table(name = "purchase_history")
 public class Purchase {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private Long bookId;
     private LocalDateTime date;
